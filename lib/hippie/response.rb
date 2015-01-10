88f8b5321c7ae71d126f0ab35582f30f7109ed6c
Martin Manelli <manelli.ml@gmail.com>
2015-01-09 21:32:02 -0300

    Add status codes

--- lib/hippie/response.rb
+++ lib/hippie/response.rb
@@ -16,8 +16,24 @@ module Hippie
       JSON.parse(@body)
     end
 
-    def succesful?
+    def information?
+      @status.between?(100, 199)
+    end
+
+    def success?
       @status.between?(200, 299)
+    end
+
+    def redirection?
+      @status.between?(300, 399)
+    end
+
+    def client_error?
+      @status.between?(400, 499)
+    end
+
+    def server_error?
+      @status.between?(500, 599)
     end
 
     def error?
