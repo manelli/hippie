1358599b8b1c61ec81a3f4332275d55ebc250f27
Martin Manelli <manelli.ml@gmail.com>
2015-01-09 22:50:42 -0300

    Use minitest

--- tests/hippie_test.rb
+++ tests/hippie_test.rb
@@ -1,89 +1,81 @@
-require 'hippie'
+# encoding: utf-8
 
-test 'GET' do
-  r = Hippie.get('http://httpbin.org/get', params: { foo: 'bar' })
+require 'minitest/autorun'
+require_relative '../lib/hippie'
 
-  assert_equal 200, r.status
-  assert_equal ['application/json'], r.headers['content-type']
-  assert_equal 'UTF-8', r.encoding.to_s
+class TestHippie < Minitest::Test
+  def test_get
+    r = Hippie.get('http://httpbin.org/get', params: { foo: 'bar' })
 
-  assert(r.json['args'] && r.json['args']['foo'] == 'bar')
+    assert_equal true, r.success?
+    assert_equal ['application/json'], r.headers['content-type']
+    assert(r.json['args'] && r.json['args']['foo'] == 'bar')
+  end
+
+  def test_post
+    r = Hippie.post('http://httpbin.org/post', data: { 'plan' => 'test' })
+
+    assert_equal true, r.success?
+    assert_equal ['application/json'], r.headers['content-type']
+    assert(r.json['form'] && r.json['form'] == { 'plan' => 'test' })
+  end
+
+  def test_put
+    r = Hippie.put('http://httpbin.org/put', data: { 'plan' => 'test' })
+
+    assert_equal true, r.success?
+    assert_equal ['application/json'], r.headers['content-type']
+    assert(r.json['form'] && r.json['form'] == { 'plan' => 'test' })
+  end
+
+  def test_delete
+    r = Hippie.delete('http://httpbin.org/delete', data: { 'plan' => 'test' })
+
+    assert_equal true, r.success?
+    assert_equal ['application/json'], r.headers['content-type']
+    assert(r.json['form'] && r.json['form'] == { 'plan' => 'test' })
+  end
+
+  def test_head
+    r = Hippie.head('http://httpbin.org/headers')
+
+    assert_equal true, r.success?
+    assert_equal ['application/json'], r.headers['content-type']
+  end
+
+  def test_options
+    # TODO
+  end
+
+  def test_patch
+    r = Hippie.patch('http://httpbin.org/patch', data: { 'plan' => 'test' })
+
+    assert_equal true, r.success?
+    assert_equal ['application/json'], r.headers['content-type']
+    assert(r.json['form'] && r.json['form'] == { 'plan' => 'test' })
+  end
+
+  def test_trace
+    # TODO
+  end
+
+  def test_basic_auth
+    r = Hippie.get('http://httpbin.org/basic-auth/u/p', auth: ['u', 'p'])
+
+    assert_equal true, r.success?
+    assert_equal true, r.json['authenticated']
+    assert_equal  'u', r.json['user']
+  end
+
+  def test_ssl
+    r = Hippie.get('https://httpbin.org/get')
+
+    assert_equal true, r.success?
+  end
+
+  def test_raises_invalid_uri
+    assert_raises(URI::InvalidURIError) {
+      Hippie.get('www.httpbin.org/get')
+    }
+  end
 end
-puts 'GET'
-
-test 'POST' do
-  r = Hippie.post('http://httpbin.org/post', data: { "plan" => "test" })
-
-  assert_equal 200, r.status
-  assert_equal ['application/json'], r.headers['content-type']
-  assert_equal 'UTF-8', r.encoding.to_s
-
-  assert(r.json['form'] && r.json['form'] == { 'plan' => 'test' })
-end
-puts 'POST'
-
-test 'PUT' do
-  r = Hippie.put('http://httpbin.org/put', data: { 'plan' => 'test' })
-
-  assert_equal 200, r.status
-  assert_equal ['application/json'], r.headers['content-type']
-  assert_equal 'UTF-8', r.encoding.to_s
-
-  assert(r.json['form'] && r.json['form'] == { 'plan' => 'test' })
-end
-puts 'PUT'
-
-test 'DELETE' do
-  r = Hippie.delete('http://httpbin.org/delete', data: { 'plan' => 'test' })
-
-  assert_equal 200, r.status
-  assert_equal ['application/json'], r.headers['content-type']
-  assert_equal 'UTF-8', r.encoding.to_s
-
-  assert(r.json['form'] && r.json['form'] == { 'plan' => 'test' })
-end
-puts 'DELETE'
-
-test 'HEAD' do
-  r = Hippie.head('http://httpbin.org/headers')
-
-  assert_equal 200, r.status
-  assert_equal ['application/json'], r.headers['content-type']
-end
-puts 'HEAD'
-
-test 'OPTIONS' do
-  # TODO
-end
-puts 'OPTIONS'
-
-test 'PATCH' do
-  r = Hippie.patch('http://httpbin.org/patch', data: { 'plan' => 'test' })
-
-  assert_equal 200, r.status
-  assert_equal ['application/json'], r.headers['content-type']
-  assert_equal 'UTF-8', r.encoding.to_s
-
-  assert(r.json['form'] && r.json['form'] == { 'plan' => 'test' })
-end
-puts 'PATCH'
-
-test 'TRACE' do
-  # TODO
-end
-puts 'TRACE'
-
-test 'Basic Auth' do
-  r = Hippie.get('http://httpbin.org/basic-auth/u/p', auth: ['u', 'p'])
-
-  assert_equal r.json['authenticated'], true
-  assert_equal r.json['user'], 'u'
-end
-puts 'Basic Auth'
-
-test 'SSL' do
-  response = Hippie.get('https://httpbin.org/get')
-
-  assert_equal 200, response.status
-end
-puts 'SSL'
